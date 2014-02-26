8dc1c31cf81044db6f26560b06dcb3b16d07cb36
Roshane Hayles <rohshane_h.rrh@hotmail.com>
2014-02-26 16:33:51 +0000

    second commit

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
-
-
-
-
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, Nothing at this URL.', 404
   
-@app.route('/')
+@app.route('/catwalk')
+def catw(name=None):
+  """ Return me template at application /me URL."""
+  return render_template('catwalk.html', name=name)
+
+@app.route('/madlibs')
 def calc(name=None):
   """ Return me template at application /me URL."""
-  return render_template('calc.html', name=name)
+  return render_template('madlibs.html', name=name)
